6e8721c909c88cdd96657373f4f4d31490bdf710
amanzew <amanzew@gmail.com>
2024-04-29 18:34:49 +0300

    initial state

--- machine.ts
+++ machine.ts
@@ -4,51 +4,16 @@ export const machine = setup({
   types: {
     context: {} as { count: number },
   },
-  actions: {
-    counter: function ({ context, event }, params) {
-      // Add your action code here
-      // ...
-    },
-  },
-  schemas: {
-    context: {
-      count: {
-        type: "number",
-        description:
-          'Generated automatically based on the key: "count" in initial context values',
-      },
-    },
-  },
+  actions: {},
 }).createMachine({
   context: {
     count: 60,
   },
-  id: "Untitled",
+  id: "taffic light",
   initial: "red",
   states: {
-    red: {
-      after: {
-        "6000": {
-          target: "yellow",
-        },
-      },
-      entry: {
-        type: "counter",
-      },
-    },
-    yellow: {
-      after: {
-        "500": {
-          target: "green",
-        },
-      },
-    },
-    green: {
-      after: {
-        "500": {
-          target: "red",
-        },
-      },
-    },
+    red: {},
+    yellow: {},
+    green: {},
   },
 });
